803992fcc109a44443ce718c705e62d22008cd88
cicero884 <cicero884@gmail.com>
2023-06-30 17:13:15 +0800

    add ntt (not done)

---
+++ HDL/syn/syn.tcl
@@ -0,0 +1,49 @@
+# Force multicore
+set_host_options -max_cores 16
+
+# Read all Files
+set top rails
+read_file -autoread -top ${top} {./rails.v} -library ${top}
+current_design ${top}
+link
+
+# Setting Clock Constraits
+source -echo -verbose ${top}.sdc
+
+# High fanout threshold
+# set high_fanout_net_threshold 0
+report_net_fanout -high_fanout
+
+uniquify
+set_fix_multiple_port_nets -all -buffer_constants [get_designs *]
+ 
+set_structure -timing true
+ 
+check_design
+
+# Synthesize (ultimate)
+compile_ultra -no_autoungroup -no_boundary_optimization -retime -gate_clock
+compile_ultra -incremental
+
+current_design [get_designs ${top}]
+ 
+remove_unconnected_ports -blast_buses [get_cells -hierarchical *]
+ 
+set bus_inference_style {%s[%d]}
+set bus_naming_style {%s[%d]}
+set hdlout_internal_busses true
+change_names -hierarchy -rule verilog
+define_name_rules name_rule -allowed {a-z A-Z 0-9 _}   -max_length 255 -type cell
+define_name_rules name_rule -allowed {a-z A-Z 0-9 _[]} -max_length 255 -type net
+define_name_rules name_rule -map {{"\\*cell\\*" "cell"}}
+define_name_rules name_rule -case_insensitive
+change_names -hierarchy -rules name_rule
+
+write -format ddc  -hierarchy -output "./syn/${top}_syn.ddc"
+write_sdf ./syn/${top}_syn.sdf
+write_file -format verilog -hierarchy -output ./syn/${top}_syn.v
+report_area > area.log
+report_timing > timing.log
+report_qor > ${top}_qor.log
+
+exit
--- HDL/tb_ntt.sv
+++ HDL/tb_ntt.sv
@@ -1,71 +1,79 @@
 `define CYCLE      50.0
 `define END_CYCLE 100000
 
+module tb_ntt();
+
+logic clk = '0,rst;
 always begin 
 	#(`CYCLE/2) clk = ~clk;
 end
 
-logic [3:0]data_in;
-logic in_en,done;
+logic [15:0]data_in;
+logic en_in, done;
 
 logic [3:0]golden;
+integer fd_in, fd_out;
 initial begin
-	reset = '0;
-	for (int i = 0; i < pat_number; i++) begin
-		fd_in = $fopen("ntt_in.txt","r");
-		fd_out = $fopen("ntt_out.txt","r");
-		if (fd_in == 0 || fd_out == 0) begin
-			$display("Failed open test data");
-			$finish;
-		end
-		else begin
-			#(`CYCLE*0.5) reset = '1; 
-			#(`CYCLE*5); #0.5; reset = '0; en = '1;
-		end
+	rst = '0;
+
+	$fsdbDumpfile("ntt.fsdb");
+	$fsdbDumpvars();
+
+	fd_in = $fopen("ntt_in.txt","r");
+	fd_out = $fopen("ntt_out.txt","r");
+	if (fd_in == 0 || fd_out == 0) begin
+		$display("Failed open test data");
+		$finish;
+	end
+	else begin
+		#(`CYCLE*0.5) rst = '1; 
+		#(`CYCLE*10); #0.5; rst = '0;
 	end
 end
 
 int cnt;
 logic busy;
-logic [3:0] mem [256];
-always_ff @(posedge clk, posedge reset) begin
-	if (reset) begin
+// TODO remove mem, stream in, wait , stream out
+always_ff @(posedge clk, posedge rst) begin
+	if (rst) begin
 		busy <= '0;
-		in_en <= '0;
+		en_in <= '0;
 		cnt <= '0;
 	end
-	if (!busy) begin
-		if ($feof(fd_in)) $finish
-		else begin
-			$fscanf(fd_in, "%h", data_in);
-			cnt <= cnt + 1;
+	else begin
+		if (!busy) begin
+			if ($feof(fd_in)) $finish;
+			else begin
+				if(!$fscanf(fd_in, "%h", data_in)) $display("WHAT");
+				cnt <= cnt + 1;
 
-			in_en <= '1;
-			shbusy <= '1;
+				en_in <= '1;
+				busy <= '1;
+			end
+		end
+		else begin
+			case(1'b1) //unique0 
+				en_in: begin
+					if(!$fscanf(fd_in, "%h", data_in)) $display("WHAT_MID");
+					cnt <= cnt + 1;
+					if (cnt == 256) en_in <= '0;
+				end
+				done: begin
+					//TODO: compare
+					busy <= '0;
+					en_in <= '0;
+					cnt <= '0;
+				end
+			endcase
 		end
 	end
-	else begin
-		unique case(1'b1)
-			in_en: begin
-				$fscanf(fd, "%h", data_in);
-				$display("%h ",data_in);
-				cnt <= cnt + 1;
-				if (cnt == 255) in_en <= '0;
-			end
-			done: begin
-				//TODO: compare
-				busy <= '0;
-				in_en <= '0;
-				cnt <= '0;
-			end
-		endcase
-	end
 end
-logic w_en ,r_en;
-logic [7:0] w_addr ,r_addr;
-logic [3:0] w_data ,r_data;
+logic en_w ,en_r;
+logic [7:0] addr_w ,addr_r;
+logic [15:0] data_w ,data_r;
 always_ff @(posedge clk) begin
-	if (w_en) mem[w_addr] <= w_data;
-	if (r_en) mem[r_addr] <= r_data;
+	if (en_w) mem[addr_w] <= data_w;
+	if (en_r) data_r <= mem[addr_r];
 end
-//ntt u_ntt//TODO
+ntt u_ntt(.*);
+endmodule
